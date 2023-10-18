e4aaf0b2f33b596ebd3f6af2b2a7873718cd9486
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2023-10-18 12:32:18 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,11 +4,5 @@ function tocaSomPom () {
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-
-//document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-//document.querySelector('#som_tecla_pom').play();
-//document.querySelector('.tecla_pom')
