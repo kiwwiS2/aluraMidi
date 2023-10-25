e168f8e6c1c9326749948b57ea266f89f94f1ebc
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2023-10-25 12:36:59 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -6,3 +6,15 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+while (contador<9) {
+    listaDeTeclas[ contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.long(contador);
+}
