6f8c0aef261eae687b89f57c5bdb87ea74ef3af7
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2023-11-01 12:35:23 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -7,6 +7,18 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
+
+wihile (contador < listaDeTeclas.length) {
+const efeito = listaDeTeclas[contador].classList[1];
+ const idAudio = "#som_"+efeito;
+ listaDeTeclas[contador].onclik = function()
+ {
+     tocaSom(idAudio);
+ }
+    contador = contador + 1;
+    consol.long(contador);
+            
+
 
 let contador = 0;
 while (contador<9) {
